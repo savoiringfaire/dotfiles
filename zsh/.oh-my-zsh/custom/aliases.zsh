41f47d97b146924b737fe7981e9e27f0326cb766
Marcus Hann <marcus.hann@spotlightsportsgroup.com>
2021-07-19 20:18:47 +0100

    Expand tmux scrollback history

--- zsh/.oh-my-zsh/custom/aliases.zsh
+++ zsh/.oh-my-zsh/custom/aliases.zsh
@@ -1,3 +1,7 @@
 alias gcs="git commit -S"
 alias gp="git push"
-alias htheader="http --follow -h --all"
+alias forcebuild="git commit --allow-empty -m 'triggerbuild' && git push"
+
+function htheader() {
+  http --follow -h --all "$@" Fastly-Debug:1
+}
